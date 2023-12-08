e658d48a891316b9b626ecb3df575961cac9fc21
Tim Laubert <t.laubert@live.de>
2023-12-08 18:26:52 +0100

    Add layout

---
+++ src/app/blog/(articles)/welcome-to-tahiti-1/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+    title: "Welcome to Tahiti Roleplay",
+    description:
+        "Welcome to our first Developer Blog, where we give you a quick rundown of the road so far and what's in store for you this year",
+};
+
+export default function ArticleLayout({ children }: any) {
+    return children;
+}
